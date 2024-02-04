363bb92567525ddcdc006001691064aaa93847e2
Mobin Fazli <m0binfae2003@gmail.com>
2024-02-04 13:04:19 -0500

    Expandify Cards

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+// // Select all elements with the class expandableCards and store them in the cards variable!
 const cards = document.querySelectorAll('.expandableCards')
 
+// Add a mouseover event listener to each card element!
 cards.forEach(card => {
+    // When the mouse is over a card, remove active class from all cards and add it to the current card!
     card.addEventListener('mouseover', () => {
         removeActiveClasses()
         card.classList.add('active')
     })
 })
 
+// Function to remove active class from all cards!
 function removeActiveClasses() {
     cards.forEach(card => {
         card.classList.remove('active')
